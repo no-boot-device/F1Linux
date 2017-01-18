09767734b7b35bff7066ed7b3ff7119205b021bc
no-boot-device <nobootdevice@national.shitposting.agency>
2017-01-18 10:43:52 -0600

    show classes in ESP, as well as buildings
    there's probably something terribly wrong with this because i just took this from my heavily modified branch of this

--- Esp.cpp
+++ Esp.cpp
@@ -8,32 +8,46 @@ void Esp::PaintTraverse()
 	{
 		CBaseEntity *baseEntity = GetBaseEntity(i);
 
-		if(baseEntity == nullptr)
-			return;
+		if(baseEntity == NULL)
+			continue;
 
 		if(baseEntity->IsDormant())
-			return;
+			continue;
 
 		if(baseEntity->IsAlive() == false)
-			return;
+			continue;
+			
+		Vector world, screen;
+		Vector min, max, origin, localOrigin;
 
+		baseEntity->GetRenderBounds(min, max);
+
+		origin = baseEntity->GetAbsOrigin();
+
+		baseEntity->GetWorldSpaceCenter(world);
+
+		if(!gDrawManager.WorldToScreen(world, screen))
+			continue;
+		
+		float yPos = screen.y+16;
+		
 		if(baseEntity->GetClientClass()->iClassID == classId::CTFPlayer)
 		{
 			// this is a player
 			
-			baseEntity->GetWorldSpaceCenter(world);
-			
-			if(gDrawManager.WorldToScreen(world, screen))
-			{
-				player_info_t info;
-				gInts.Engine->GetPlayerInfo(i, &info);
+			player_info_t info;
+			gInts.Engine->GetPlayerInfo(i, &info);
 
-				float yPos = screen.y;
-				
-				gDrawManager.DrawString(screen.x, yPos, 0xFFFFFFFF, "%s", info.name);
-				yPos+=gDrawManager.GetESPHeight();
-				gDrawManager.DrawString(screen.x, screen.y, 0xFFFFFFFF, "%i", baseEntity->GetHealth());
-				yPos+=gDrawManager.GetESPHeight();
+			gDrawManager.DrawString(screen.x, yPos, gDrawManager.dwGetTeamColor(baseEntity->GetTeam()), "%s", info.name);
+			yPos+=gDrawManager.GetESPHeight();
+			gDrawManager.DrawString(screen.x, yPos, gDrawManager.dwGetTeamColor(baseEntity->GetTeam()), "(%s)", baseEntity->GetClassName());
+			yPos+=gDrawManager.GetESPHeight();
+			gDrawManager.DrawString(screen.x, screen.y, gDrawManager.dwGetTeamColor(baseEntity->GetTeam()), "%i", baseEntity->GetHealth());
+			yPos+=gDrawManager.GetESPHeight();
+		} else {
+			ClientClass* bec = baseEntity->GetClientClass();
+			if((bec->iClassID== classId::CObjectDispenser || bec->iClassID== classId::CObjectSapper || bec->iClassID== classId::CObjectSentrygun || bec->iClassID== classId::CObjectTeleporter)) {
+				gDrawManager.DrawString(screen.x, screen.y, COLORCODE(0,0,0,255), "%s", bec->chName);
 			}
 		}
 	}
